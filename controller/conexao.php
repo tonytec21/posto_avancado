5ef314634870ea77ab6a58a47af929f3b08bf7e8
tonytec9 <88214774+tonytec21@users.noreply.github.com>
2022-05-06 16:46:18 -0300

    Update conexao.php

--- controller/conexao.php
+++ controller/conexao.php
@@ -1,7 +1,7 @@
 <?php
 
 function conectar(){
-
+ 
 try{
 
 $pdo = new PDO("mysql:host=localhost;dbname=bookc", "root","");
@@ -17,6 +17,34 @@ catch (PDOException $e) {
 
 return $pdo;
 }
+
+
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'bookc');
+define('DB_PORT', '3306');
+define('DB_CHARSET', 'utf8mb4');
+
+ 
+define('DB_NAME_2', 'bookc_auditoria');
+
+
+function conexao_logs(){
+
+    try{
+    $pdo = new PDO("mysql:host=" . DB_SERVER . ";charset=utf8;port=" . DB_PORT . ";dbname=" . DB_NAME_2, DB_USERNAME, DB_PASSWORD);
+        // Set the PDO error mode to exception
+       $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $e){
+        die("ERROR: Could not connect. " . $e->getMessage());
+    }
+    
+    return $pdo;
+    }
+
+
 
 
  ?>
